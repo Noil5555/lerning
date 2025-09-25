c63efc241d2a5c14ceca45a85df991eaffe20513
Noil5555 <lionlucawitt@gmail.com>
2025-09-25 21:13:47 +0200

    Add files via upload
    added game

--- Program.cs
+++ Program.cs
@@ -2,97 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace hallo
+namespace Holz
 {
     internal class Program
     {
         static void Main(string[] args)
-        {   // Einsehen & Einzahlem & Abheben
-            string logging = "1111";
-            string username = "";
-            string userimpout = "";
-            int pin = 0;
+        {
+            string logging = "";
+            string eiche = "eiche";
+            string lärche = "lärche";
+            string fichte = "fichte";
+            string buche = "buche";
+            string birke = "birke";
+            string tanne = "tanne";
+            string userinput = "";
 
-
-            while (logging == "1111")
+            List<string> baume = new List<string>
             {
-                Console.WriteLine(
-                    "Was möchtest du machen?   1) login   2) mit einem Mitarbeiter sprechen   3) Terminmwunsch    4) Exit");
-                logging = Console.ReadLine();
-            }
-
-            while (logging == "1")
-            {
-                Console.WriteLine("1) einloggen    2) regestrieren   3) exit");
-                userimpout = Console.ReadLine();
-
-                if (userimpout == "1")
-                {
-                    bnak myKonto = new bnak(moneten: 100.0f,passwd: pin, user: username);
-                    myKonto.login(username, pin);
-                    
-                    Console.WriteLine("was möchtest du machen?   1) geld einzahlen (lecker)   2) geld ansehen (gut)   3) geld abheben (ekelhaft)");
-                    userimpout = Console.ReadLine();
-
-                    if (userimpout == "1")
-                    {
-                        myKonto.einzahlen();
-                        logging = "1111";
-                    } else if (userimpout == "2")
-                    {
-                        myKonto.einsehen();
-                        logging = "1111";
-                    } else if (userimpout == "3")
-                    {
-                        myKonto.cashout();
-                        logging = "1111";
-                    }
-                    else
-                    {
-                        logging = "1111";
-                    }
-                    
-                }   
-                else if (userimpout == "2")
-                {
-                    Console.WriteLine("Was soll dein Username sein?");
-                    username = Console.ReadLine();
-                    
-                    Console.WriteLine("Was soll deine Pin sein?");
-                    pin = int.Parse(Console.ReadLine());
-                    
-                    Console.WriteLine("Schreibe Quit, um den Screen zu verlassen");
-                    userimpout = Console.ReadLine().ToLower();
-                    
-                    if (userimpout == "quit")
-                    {
-                        Console.WriteLine("Was möchtest du machen?   1) login   2) mit einem Mitarbeiter sprechen   3) Terminmwunsch    4) Exit");
-                        logging = Console.ReadLine(); 
-                    }
-                }
-            }
-
-            while (logging == "4")
-            {
-                Console.WriteLine("Verpisst dich");
-                logging = "0";
-            }
-
-            while (logging == "2")
-            {
-                Console.WriteLine("Verpisst dich");
-                logging = "0";
-            }
-
-            while (logging == "3")
-            {
-                Console.WriteLine("Verpisst dich");
-                logging = "0";
-            }
+                "Eiche",
+                "Lärche",
+                "Fichte",
+                "Buche",
+                "Birke",
+                "Tanne"
+            };
             
+            Random r = new Random();
+
+            Console.WriteLine("Willkommen in dem Game der Zimmerer\n"+
+                              "Das Ziehl dieses Spiels ist es das Holz, an Hand des Ausehens den richtigen Namen zuzurodnen\n"+
+                              "Wenn du bereit bist zum Start schreibe: START zum starten oder EXIT um das Programm zu beenden");
+
+            logging = Console.ReadLine().ToLower();
+
+            if (logging == "start")
+            {
+
+            } else if (logging == "exit")
+            {
+                Console.WriteLine("Auf wieder sehen");
+                logging = "";
+            }
+            else
+            {
+                Console.WriteLine("Bitte schreibe entweder START um zu starten oder EXIT zum Programm beenden");
+                logging = Console.ReadLine().ToLower();
+            }
+
+            arten vergleicher = new arten();
+            
+            while (baume.Count > 0)
+            {
+                int index = r.Next(baume.Count);
+                string temp = baume[index];
+
+                Console.WriteLine("Was ist dieses Holz? " + temp);
+                baume.RemoveAt(index);
+
+                userinput = Console.ReadLine().ToLower();
+                
+                if (vergleicher.)
+            }
         }
-        
     }
 }
